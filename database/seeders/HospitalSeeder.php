19f556f6b516a6577f1931e334fa4af9b8e6a87f
ohagi14 <furikake.hs@gmail.com>
2023-02-02 16:20:57 +0900

    ダミーデータ生成

--- database/seeders/HospitalSeeder.php
+++ database/seeders/HospitalSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class HospitalSeeder extends Seeder
 {
@@ -14,6 +16,31 @@ class HospitalSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('hospitals')->insert([
+            [
+                'title' => '動物病院タイトル１',
+                'sub_title' => 'ドウブツビョウインタイトルイチ',
+                'contents' => '詳細内容',
+                'sample_num' => '100',
+            ],
+            [
+                'title' => '動物病院タイトル2',
+                'sub_title' => 'ドウブツビョウインタイトルニ',
+                'contents' => '詳細内容',
+                'sample_num' => '200',
+            ],
+            [
+                'title' => '動物病院タイトル３',
+                'sub_title' => 'ドウブツビョウインタイトルサン',
+                'contents' => '詳細内容',
+                'sample_num' => '110',
+            ],
+            [
+                'title' => '動物病院サンプル',
+                'sub_title' => 'ドウブツビョウインサンプル',
+                'contents' => '詳細内容',
+                'sample_num' => '110',
+            ]
+        ]);
     }
 }
