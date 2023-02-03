1dd8d430808b211152e38f3ab23934ac34ddfafc
ohagi14 <furikake.hs@gmail.com>
2023-02-03 16:20:55 +0900

    ♻️

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -1,9 +1,10 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-    <div v-if="$page.props.flash.status === 'success'" class="bg-blue-300 text-white p-4">
+    <div
+        v-if="$page.props.flash.status === 'success'"
+        class="bg-blue-300 text-white p-4"
+    >
         {{ $page.props.flash.message }}
     </div>
 </template>
