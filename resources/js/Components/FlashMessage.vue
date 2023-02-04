b990be68ce7b44fefb7c8c3635b6d94b6d18de59
ohagi14 <furikake.hs@gmail.com>
2023-02-04 09:40:28 +0900

    indent_sizeをtabに変更

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -1,10 +1,10 @@
 <script setup></script>
 
 <template>
-    <div
-        v-if="$page.props.flash.status === 'success'"
-        class="bg-blue-300 text-white p-4"
-    >
-        {{ $page.props.flash.message }}
-    </div>
+	<div
+		v-if="$page.props.flash.status === 'success'"
+		class="bg-blue-300 text-white p-4"
+	>
+		{{ $page.props.flash.message }}
+	</div>
 </template>
