3c30100b2d071f6ed4516ab5542770cb47e4a582
ohagi14 <furikake.hs@gmail.com>
2023-02-15 10:28:59 +0900

    ♻️ propsにてfill色変更をv-binにて対応

--- resources/js/Components/Icon/Search.vue
+++ resources/js/Components/Icon/Search.vue
@@ -1,3 +1,10 @@
+<script setup>
+const	props = defineProps({
+	color: {
+		default: '#455f83',
+	}
+})
+</script>
 <template>
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
@@ -9,7 +16,7 @@
 			id="ic_search_24px"
 			d="M15.864,14.321h-.813l-.288-.278a6.7,6.7,0,1,0-.72.72l.278.288v.813L19.467,21,21,19.467Zm-6.175,0A4.631,4.631,0,1,1,14.321,9.69,4.625,4.625,0,0,1,9.69,14.321Z"
 			transform="translate(-3 -3)"
-			fill="#455f83"
+			:fill="props.color"
 		/>
 	</svg>
 </template>
--- resources/js/Components/Tab/TabBasic.vue
+++ resources/js/Components/Tab/TabBasic.vue
@@ -36,19 +36,7 @@ import i_Plus from "@/Components/Icon/Plus.vue";
 				</div>
 			</div>
 			<button class="btn shadow-md flex items-center justify-center bg-main text-white mt-8 w-[336px] h-16 mx-auto gap-x-2 font-semibold">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="18"
-					height="18"
-					viewBox="0 0 18 18"
-				>
-					<path
-						id="ic_search_24px"
-						d="M15.864,14.321h-.813l-.288-.278a6.7,6.7,0,1,0-.72.72l.278.288v.813L19.467,21,21,19.467Zm-6.175,0A4.631,4.631,0,1,1,14.321,9.69,4.625,4.625,0,0,1,9.69,14.321Z"
-						transform="translate(-3 -3)"
-						fill="#fff"
-					/>
-				</svg>
+				<i_Search color="#fff" />
 				検索する
 			</button>
 		</div>
