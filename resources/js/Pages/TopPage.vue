0a5946002006faa4a5efd206d1eebb26cfefdb9b
ohagi14 <furikake.hs@gmail.com>
2023-02-16 17:53:37 +0900

    ♻️ 都道府県、市区町村 呼び出し表示対応

--- resources/js/Pages/TopPage.vue
+++ resources/js/Pages/TopPage.vue
@@ -12,14 +12,19 @@ defineProps({
 </script>
 <template>
 	<Head title="Topページ" />
-	<ul v-for="(pref, i) in prefs" v-bind:key="pref.id">
+	<ul class="pl-10" v-for="(pref, i) in prefs" v-bind:key="pref.id">
 		<li>{{ pref.name }}</li>
 		<ul class="cities pl-4">
 			<template v-for="(cities, n) in prefs[i].cities" v-bind:key="cities.id">
-				<li class="-ml-4" v-if="n === 0">{{ pref.name + "全域" }}</li>
-				<li>
-					{{ cities.name }}
+				<li v-if="cities.words">
+					{{cities.name}}
+					<template v-for="(words, v) in prefs[i].cities[n].words" v-bind:key="words.id">
+						<li class="pl-4">
+							{{words.name}}
+						</li>
+					</template>
 				</li>
+				<li v-else>{{cities.name}}</li>
 			</template>
 		</ul>
 	</ul>
