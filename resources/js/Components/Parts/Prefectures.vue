5b2100d42de9941b3e5f851562d28f07bc2658d0
ohagi14 <furikake.hs@gmail.com>
2023-02-18 12:35:18 +0900

    都道府県 チェックボックスレイアウト作成中

--- resources/js/Components/Parts/Prefectures.vue
+++ resources/js/Components/Parts/Prefectures.vue
@@ -2,29 +2,57 @@
 defineProps({
 	prefectures: Array,
 });
-
 </script>
 <template>
-	<ul>
-		<li v-for="pref in prefectures" v-bind:key="pref.id">{{ pref.name }}</li>
-	</ul>
-	<ul class="pl-10" v-for="(pref, i) in prefectures" v-bind:key="pref.id">
-		<li>{{ pref.name }}</li>
-		<ul class="cities pl-4">
-			<template v-for="(cities, n) in prefectures[i].cities" v-bind:key="cities.id">
-				<li v-if="cities.words">
-					{{ cities.name }}
-					<template
-						v-for="words in prefectures[i].cities[n].words"
-						v-bind:key="words.id"
-					>
-						<li class="pl-4">
-							{{ words.name }}
-						</li>
-					</template>
-				</li>
-				<li v-else>{{ cities.name }}</li>
-			</template>
-		</ul>
-	</ul>
+	<aside class="prefectures">
+		<div class="wrap">
+			<div class="prefectures-tab">
+				<ul>
+					<li v-for="pref in prefectures" v-bind:key="pref.id">
+						{{ pref.name }}
+					</li>
+				</ul>
+			</div>
+			<div class="prefectures-content">
+				<template v-for="(pref, i) in prefectures" v-bind:key="pref.id">
+						<template
+							v-for="(cities, n) in prefectures[i].cities"
+							v-bind:key="cities.id"
+						>
+							<template v-if="cities.words">
+								<p>{{ pref.name + "ー" + cities.name }}</p>
+								<ul class="grid grid-cols-4 gap-5 my-5">
+									<li>
+										<input type="checkbox" :id="'cities-' + cities.id" />
+										<label :for="'cities-' + cities.id"
+											>{{ cities.name }}全域</label
+										>
+									</li>
+									<template
+										v-for="words in prefectures[i].cities[n].words"
+										v-bind:key="words.id"
+									>
+										<li>
+											<input type="checkbox" :id="'words-' + words.id" />
+											<label :for="'words-' + words.id">{{
+												cities.name + words.name
+											}}</label>
+										</li>
+									</template>
+								</ul>
+							</template>
+							<template v-else>
+								<!-- <p v-if="n === 1">{{ pref.name + "ー 市部" }}</p> -->
+								<ul>
+									<li>
+										<input type="checkbox" :id="'place-' + cities.id" />
+										<label :for="'place-' + cities.id">{{ cities.name }}</label>
+									</li>
+								</ul>
+							</template>
+						</template>
+				</template>
+			</div>
+		</div>
+	</aside>
 </template>
