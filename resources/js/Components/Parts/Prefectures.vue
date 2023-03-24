6e778aa8852ddb69c022138e5d9938c6e32523cc
ohagi14 <furikake.hs@gmail.com>
2023-03-24 09:00:01 +0900

    モーダル表示、デフォルト北海道表示対応

--- resources/js/Components/Parts/Prefectures.vue
+++ resources/js/Components/Parts/Prefectures.vue
@@ -9,6 +9,17 @@ const emit = defineEmits(["hospitalCheck"]);
 const onChangeHospitalPre = () => {
 	emit("hospitalCheck", { checkValue });
 };
+const clickPref = (e) => {
+	const targetTabEn = document.querySelectorAll("[data-tabEn]");
+	for (let i = 0; i < targetTabEn.length; i++) {
+		const tab = targetTabEn[i].dataset.taben;
+		if (tab === e) {
+			targetTabEn[i].style.display = "block";
+		} else {
+			targetTabEn[i].style.display = "none";
+		}
+	}
+};
 </script>
 <template>
 	<aside class="prefectures">
@@ -18,15 +29,56 @@ const onChangeHospitalPre = () => {
 				<ul>
 					<li
 						class="font-semibold"
-						v-for="pref in prefectures"
-						v-bind:key="pref.id"
+						v-for="prefecture in prefectures"
+						:key="prefecture.id"
+						@click="clickPref(prefecture.en)"
 					>
-						{{ pref.name }}
+						{{ prefecture.name }}
 					</li>
 				</ul>
 			</div>
 			<div class="prefectures-content">
-				<template v-for="(pref, i) in prefectures" v-bind:key="pref.id">
+				<template v-for="prefecture in prefectures" :key="prefecture.id">
+					<ul :data-tabEn="prefecture.en" v-show="prefecture.id == '01'">
+						<template v-for="cities in prefecture.cities" :key="cities.id">
+							<template v-if="cities.words && cities.words.length > 0">
+								<li>
+									<p class="c-TabText80">{{ cities.title }}</p>
+									<ul class="grid grid-cols-4 gap-5 my-5 text-sm">
+										<li v-for="ward in cities.words" :key="ward">
+											<input
+												type="checkbox"
+												:id="'ward-' + ward.id"
+												:value="prefecture.name + ward.name"
+												@change="onChangeHospitalPre"
+												v-model="checkValue"
+											/>
+											<label :for="'ward-' + ward.id">{{
+												cities.name + ward.name
+											}}</label>
+										</li>
+									</ul>
+								</li>
+							</template>
+							<template v-else>
+								<li>
+									<p class="c-TabText80">{{ prefecture.title }}</p>
+								</li>
+								<li>
+									<input
+										type="checkbox"
+										:id="'cities-' + cities.id"
+										:value="cities.name"
+										@change="onChangeHospitalPre"
+										v-model="checkValue"
+									/>
+									<label :for="'cities-' + cities.id">{{ cities.name }}</label>
+								</li>
+							</template>
+						</template>
+					</ul>
+				</template>
+				<!-- <template v-for="(pref, i) in prefectures" v-bind:key="pref.id">
 					<template
 						v-for="(cities, n) in prefectures[i].cities"
 						v-bind:key="cities.id"
@@ -81,7 +133,7 @@ const onChangeHospitalPre = () => {
 							</ul>
 						</template>
 					</template>
-				</template>
+				</template> -->
 			</div>
 		</div>
 	</aside>
