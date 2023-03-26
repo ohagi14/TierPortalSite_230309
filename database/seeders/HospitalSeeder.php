c4f69682db1f879642e3eda10542c448090fbd59
ohagi14 <furikake.hs@gmail.com>
2023-03-27 08:33:09 +0900

    ダミーデータを修正

--- database/seeders/HospitalSeeder.php
+++ database/seeders/HospitalSeeder.php
@@ -9,49 +9,49 @@ use Illuminate\Support\Facades\Hash;
 
 class HospitalSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        DB::table('hospitals')->insert([
-            [
-                'title' => '動物病院タイトル１',
-                'sub_title' => 'ドウブツビョウインタイトルイチ',
-                'contents' => '詳細内容',
-                'sample_num' => '100',
-                'prefecture' => '札幌市',
-            ],
-            [
-                'title' => '動物病院タイトル2',
-                'sub_title' => 'ドウブツビョウインタイトルニ',
-                'contents' => '詳細内容',
-                'sample_num' => '200',
-								'prefecture' => '札幌市中央区',
-            ],
-            [
-                'title' => '動物病院タイトル３',
-                'sub_title' => 'ドウブツビョウインタイトルサン',
-                'contents' => '詳細内容',
-                'sample_num' => '110',
-								'prefecture' => '札幌市北区',
-            ],
-            [
-                'title' => '動物病院サンプル4',
-                'sub_title' => 'ドウブツビョウインサンプル',
-                'contents' => '詳細内容',
-                'sample_num' => '110',
-								'prefecture' => '函館市',
-            ],
-						[
-							'title' => '動物病院サンプル5',
-							'sub_title' => 'ドウブツビョウインサンプル',
-							'contents' => '詳細内容',
-							'sample_num' => '110',
-							'prefecture' => '盛岡市',
-					],
-        ]);
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('hospitals')->insert([
+			[
+				'title' => 'ノルド動物病院',
+				'sub_title' => 'ドウブツビョウインタイトルイチ',
+				'contents' => '詳細内容',
+				'sample_num' => '100',
+				'prefecture' => '北海道札幌市清田区北野3条3丁目1-1',
+			],
+			[
+				'title' => '桑園動物病院',
+				'sub_title' => 'ソウエンドウブツビョウイン',
+				'contents' => '詳細内容',
+				'sample_num' => '200',
+				'prefecture' => '北海道札幌市中央区北8条西19丁目35番地10',
+			],
+			[
+				'title' => 'ガーデン動物病院',
+				'sub_title' => 'ガーデンドウブツビョウイン',
+				'contents' => '詳細内容',
+				'sample_num' => '110',
+				'prefecture' => '北海道札幌市中央区北2条西25-2-1',
+			],
+			[
+				'title' => '円山動物病院',
+				'sub_title' => 'マルヤマドウブツビョウイン',
+				'contents' => '詳細内容',
+				'sample_num' => '110',
+				'prefecture' => '北海道札幌市中央区宮の森2条13丁目12-21',
+			],
+			[
+				'title' => '榎本犬猫病院',
+				'sub_title' => 'エノモトイヌネコビョウイン',
+				'contents' => '詳細内容',
+				'sample_num' => '110',
+				'prefecture' => '神奈川県相模原市緑区寸沢嵐848-1',
+			],
+		]);
+	}
 }
